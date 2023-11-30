3a4d8dc47ef44a9e5e860068b21c320e11622f58
Dylan Marin <dylanjmmarin@gmail.com>
2023-11-30 17:46:40 -0500

    git ignore

---
+++ app.js
@@ -0,0 +1,48 @@
+import express from "express";
+import "dotenv/config.js";
+import Hello from "./hello.js";
+import Lab5 from "./lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+import "dotenv/config";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+
+mongoose.connect(CONNECTION_STRING);
+
+const app = express();
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    })
+);
+
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
+
+app.use(express.json());
+
+CourseRoutes(app);
+ModuleRoutes(app);
+UserRoutes(app);
+
+Lab5(app);
+Hello(app);
+
+app.listen(process.env.PORT || 4000);
