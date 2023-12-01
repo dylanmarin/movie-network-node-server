f0ed04e72456846c787103831f0241e6b5e9404b
Dylan Marin <dylanjmmarin@gmail.com>
2023-12-01 18:45:17 -0500

    add new review

--- reviews/schema.js
+++ reviews/schema.js
@@ -1,12 +1,19 @@
 import mongoose, {Schema} from "mongoose";
 
 const reviewSchema = new mongoose.Schema({
-        userId: {type: Schema.Types.ObjectId, required: true},
-        username: {type: String, required: true},
-        movieId: {type: String, required: true},
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "users",
+            required: true
+        },
+        movieId: {
+            type: Number,
+            ref: "movieDetails",
+            required: true
+        },
         reviewText: {type: String, required: true},
         rating: {type: Number, required: true},
-        date: {type: Date, required: true},
+        date: {type: Date, required: true, default: new Date().getTime()},
     },
     {collection: "reviews"});
 export default reviewSchema;
