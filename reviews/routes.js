b5ccfd3ea3a77cd37dcaf12745b24b01d985f552
Dylan Marin <dylanjmmarin@gmail.com>
2023-12-08 14:55:16 -0500

    making reviews routes all trycatch

--- reviews/routes.js
+++ reviews/routes.js
@@ -2,32 +2,51 @@ import * as dao from "./dao.js";
 
 function ReviewsRoutes(app) {
     const findAllReviews = async (req, res) => {
-        res.send(await dao.findAllReviews());
+        try {
+            res.send(await dao.findAllReviews());
+        } catch (e) {
+            res.send(null);
+        }
     }
 
     const createReview = async (req, res) => {
-        const review = await dao.createReview(req.body);
-        res.send(review)
+        try {
+            const review = await dao.createReview(req.body);
+            res.send(review)
+        } catch (e) {
+            res.send(null)
+        }
     }
 
     const findReviewById = async (req, res) => {
         const {reviewId} = req.params;
-        const review = await dao.findReviewById(reviewId);
-        res.send(review);
+        try {
+            const review = await dao.findReviewById(reviewId);
+            res.send(review);
+        } catch (e) {
+            res.send(null);
+        }
     }
 
     const updateReview = async (req, res) => {
         const {reviewId} = req.params;
-        const review = await dao.updateReview(reviewId, req.body);
-        res.send(review);
+        try {
+            const review = await dao.updateReview(reviewId, req.body);
+            res.send(review);
+        } catch (e) {
+            res.send(null);
+        }
     }
 
     const deleteReview = async (req, res) => {
         const {reviewId} = req.params;
-        const status = await dao.deleteReview(reviewId);
-        res.send(status);
+        try {
+            const status = await dao.deleteReview(reviewId);
+            res.send(status);
+        } catch (e) {
+            res.send(null);
+        }
     }
-
 
     app.get("/api/reviews", findAllReviews);
     app.post("/api/reviews", createReview);
